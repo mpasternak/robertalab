086c5919b30843401b3f7735f10db8f9706d0d0b
Artem Vinokurov <artem.vinokurov@iais.fraunhofer.de>
2017-12-20 14:06:52 +0100

    #682 added new hal zip file and tweaked the source path

--- OpenRobertaParent/OpenRobertaServer/src/main/java/de/fhg/iais/roberta/javaServer/restServices/robot/RobotDownloadProgram.java
+++ OpenRobertaParent/OpenRobertaServer/src/main/java/de/fhg/iais/roberta/javaServer/restServices/robot/RobotDownloadProgram.java
@@ -62,6 +62,9 @@ public class RobotDownloadProgram {
                     filePath = this.pathToCrosscompilerBaseDir + token + "/target";
                     break;
                 case "Nao":
+                    fileName = programName + ".py";
+                    filePath = this.pathToCrosscompilerBaseDir + token + "/" + programName + "/src";
+                    break;
                 case "ev3dev":
                     fileName = programName + ".py";
                     filePath = this.pathToCrosscompilerBaseDir + token + "/src";
