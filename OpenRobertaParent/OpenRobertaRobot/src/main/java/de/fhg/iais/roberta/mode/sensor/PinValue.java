ead2d38c255c95e7f7271e7539fa0718d44e6682
Evgeniya Ovchinnikova <elhed@yandex.ru>
2017-12-29 19:24:35 +0100

    #684 fixed color light sensor for EV3, NXT and Botnroll, fixed infrared sensor (presence) for Botnroll and EV3; fixed codegeneration for the sensors those were broken in Calliope and Microbit (that requires some fixed in simulation and tests, that is why not commiting to develop)

---
+++ OpenRobertaParent/OpenRobertaRobot/src/main/java/de/fhg/iais/roberta/mode/sensor/PinValue.java
@@ -0,0 +1,27 @@
+package de.fhg.iais.roberta.mode.sensor;
+
+import de.fhg.iais.roberta.inter.mode.sensor.IPinValue;
+
+/**
+ * This enumeration contain all types of sensors that are used in <b>mbedSensors_pin_getSample</b> Blockly block.
+ */
+public enum PinValue implements IPinValue {
+    ANALOG( "AnalogValue" ),
+    DIGITAL( "DigitalValue" ),
+    PULSE_HIGH( "PulseHigh" ),
+    PULSE_LOW( "PulseLow" ),
+    //PIN_PULSE_LOW( "PulseLow" ),
+    //PIN_PULSE_HIGH( "PulseHigh" ),
+    DEFAULT;
+
+    private final String[] values;
+
+    private PinValue(String... values) {
+        this.values = values;
+    }
+
+    @Override
+    public String[] getValues() {
+        return this.values;
+    }
+}
